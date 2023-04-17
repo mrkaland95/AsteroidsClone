50b03236632868ad6c4e3aa0d2c95e8616611900
Steffen Arnesen Kaland <mrkaland95@gmail.com>
2023-04-17 20:54:08 +0200

    Write documentation.

--- src/main/java/no/uib/inf101/sem2/controller/ControllableAsteroidModel.java
+++ src/main/java/no/uib/inf101/sem2/controller/ControllableAsteroidModel.java
@@ -4,11 +4,28 @@ import no.uib.inf101.sem2.model.GameState;
 
 public interface ControllableAsteroidModel {
     GameState getGameState();
+
+    /**
+     * Accelerates the player's ship
+     * @param deltaTime
+     */
     void accelerateShip(double deltaTime);
+
+    /**
+     * Rotates the player's ship by the given angle.
+     * @param deltaTime
+     * @param angle
+     */
     void rotateShip(double deltaTime, float angle);
+
+    /**
+     * Fires a projectile from the player's ship that can kill adversaries.
+     * @param deltaTime
+     */
     void fireFromShip(double deltaTime);
 
-    /** Updates the state of the model, i.e object's position etc.
+    /** Method responsible for updating the state of the game, i.e
+     * Moving the game objects, perform player actions, remove killed game object.
      *
      * @param deltaTime The time since the last frame was rendered.
      */
